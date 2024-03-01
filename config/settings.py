43c5e578ea0935d98a266302dbdb0bd055e3ced4
shaxboz-1805 <islomovshaxboz740@gmail.com>
2024-03-01 14:49:34 +0500

    all_page

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fl)a0$ry%d6-a2$m6q05j^@abfw9#r^t2&x_sq@1c2nwr0f%(6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("oxuapp.urls")),
     path("users/", include("users.urls"))
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]   + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)  
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
